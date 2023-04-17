291704b87d216c6fd88efccbc55c094b8266c717
Kittison <touchtool@hotmail.com>
2023-04-17 19:32:26 +0700

    axios get item pages

--- packages/ecommerce/src/pages/App.tsx
+++ packages/ecommerce/src/pages/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from '../components/Navbar'
 import Card from '../components/Card'
 import gardenBackground from '../assets/garden-wallpaper.jpg'
+import axiosBaseUrl from '../axiosBaseUrl'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let { data } = await axiosBaseUrl.axiosItemsInstance.get(`/api/Items`)
+        setData(data)
+        console.log(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchData();
+  }, [])
+  
+  const typeBook = [{type: "Action"}, {type: "Fantasy"}, {type: "Horror"}]
 
   return (
     <div className="flex max-h-screen max-w-screen">
@@ -18,15 +35,16 @@ function App() {
 
         {/* For loop Selling section */}
         <div className="container mx-auto text-2xl font-bold text-blue-600 md:text-4xl">
-            <h1 className='py-2'> Decorative tree </h1>
-            <div className='flex flex-col rounded md:max-w-full md:grid md:grid-cols-3 lg:grid-cols-5 md:gap-4 gap-y-6'>
-              <Card name='one' price={23} img="https://th.bing.com/th?id=ORMS.cf3843c70cea21bad263b63d5aee9e21&pid=Wdp&w=612&h=304&qlt=90&c=1&rs=1&dpr=1.25&p=0"></Card>
-              <Card name='two' price={32}></Card>
-              <Card></Card>
-              <Card></Card>
-              <Card></Card>
-              <Card></Card>
+          {typeBook?.map((item: any, index: number) =>
+            <div>
+              <h1 className='py-2'> {item?.type} </h1>
+              <div className='flex flex-col rounded md:max-w-full md:grid md:grid-cols-3 lg:grid-cols-5 md:gap-4 gap-y-6'>
+              {data?.map((itemIn: any, index: number) => itemIn?.type == item?.type &&
+                <Card name={itemIn?.name} price={item?.price} img="https://th.bing.com/th?id=ORMS.cf3843c70cea21bad263b63d5aee9e21&pid=Wdp&w=612&h=304&qlt=90&c=1&rs=1&dpr=1.25&p=0"></Card>
+              )}
+              </div>
             </div>
+          )}
         </div>
       </div>
     </div>
